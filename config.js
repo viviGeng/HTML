8fa1e1036e22a58279fb6f81e489e9a6a7bcbebe
Ruoxin Geng <ruoxingeng@CNruoxingeng.local>
2018-12-24 18:44:28 +0800

    move randomly

---
+++ config.js
@@ -0,0 +1,27 @@
+
+var sheep = [
+    {
+        "name": "sheep1"
+    },
+    {
+        "name": "sheep2"
+    },
+    {
+        "name": "sheep3"
+    },
+    {
+        "name": "sheep4"
+    },
+    {
+        "name": "sheep5"
+    },
+    {
+        "name": "sheep6"
+    },
+    {
+        "name": "sheep7"
+    },
+    {
+        "name": "sheep8"
+    }
+]
